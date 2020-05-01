8d7a8a3b862b4a9029288ae4f30963b5fdcdca76
chirchir <chirchir7370@gmail.com>
2020-05-01 10:12:15 +0300

    created user table

---
+++ migrations/20200501064331-create-user.js
@@ -0,0 +1,55 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: Sequelize.STRING,
+        validate: {
+          min: {
+            args: 6,
+            msg: 'password must be more than 6 characters',
+          },
+        },
+      },
+      isSeller: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      isBuyer: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  },
+};
---
+++ models/user.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          min: {
+            args: 6,
+            msg: 'password must be more than 6 characters',
+          },
+        },
+      },
+      isSeller: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      isBuyer: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+    },
+    {}
+  );
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+  return User;
+};
