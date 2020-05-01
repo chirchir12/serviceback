c56f90842a0d9436004b91da246416166dbad773
chirchir <chirchir7370@gmail.com>
2020-05-01 12:26:28 +0300

    added job table

---
+++ models/job.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Job = sequelize.define(
+    'Job',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUID4,
+      },
+      title: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      slug: DataTypes.STRING,
+      deadline: DataTypes.DATE,
+      budget: DataTypes.FLOAT,
+    },
+    {}
+  );
+  Job.associate = function (models) {
+    // associations can be defined here
+  };
+  return Job;
+};
