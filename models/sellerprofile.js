6c24b79df920118ec77495f1ba214cf55241fa06
chirchir <chirchir7370@gmail.com>
2020-05-01 10:31:26 +0300

    added sellerprofile

---
+++ models/sellerprofile.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const SellerProfile = sequelize.define(
+    'SellerProfile',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+      },
+      profession: {
+        type: DataTypes.STRING,
+      },
+      experience: {
+        type: DataTypes.INTEGER,
+      },
+      phone: {
+        type: DataTypes.INTEGER,
+        validate: {
+          isNumeric: true,
+        },
+      },
+      title: {
+        type: DataTypes.STRING,
+      },
+      bio: {
+        type: DataTypes.TEXT,
+      },
+    },
+    {}
+  );
+  SellerProfile.associate = function (models) {
+    // associations can be defined here
+  };
+  return SellerProfile;
+};
